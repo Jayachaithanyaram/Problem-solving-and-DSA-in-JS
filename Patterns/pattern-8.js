6817d5e4c466d900300ee2892556842fed6e31d5
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-06-06 17:11:37 +0530

    solved type-8 Pattern Problem

---
+++ Patterns/pattern-8.js
@@ -0,0 +1,32 @@
+/*
+    *********
+     *******
+      *****
+       ***
+        *
+*/
+
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+function pattern(n) {
+    for (let i = 0; i < n; i++) {
+        let res = "";
+        for (let j = 0; j < i; j++) {
+            res += " ";
+        }
+        for (let j = 0; j < 2 * n - (2 * i + 1); j++) {
+            res += "*"
+        }
+        for (let j = 0; j < i; j++) {
+            res += " ";
+        }
+        console.log(res)
+    }
+}
+rl.question("Enter rows :", n => {
+    pattern(n);
+    rl.close();
+})
