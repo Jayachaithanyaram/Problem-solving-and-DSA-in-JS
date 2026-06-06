eac30888095dac3757c261d2b139b7bf879ec824
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-06-06 17:12:51 +0530

    solved type-11 Pattern Problem

---
+++ Patterns/pattern-11.js
@@ -0,0 +1,30 @@
+/**
+ * Sample Output:
+ *     1
+ *     01
+ *     101
+ *     0101
+ *     10101
+ */
+
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+function pattern(n) {
+
+    for (let i = 1; i <=n; i++) {
+        let res = ""
+        let start = (i % 2 === 0) ? 0 : 1;
+        for (let j = 1; j <= i; j++) {
+            res += start
+            start = 1 - start
+        }
+        console.log(res)
+    }
+}
+rl.question("Enter rows :", n => {
+    pattern(parseInt(n, 10));
+    rl.close();
+})
