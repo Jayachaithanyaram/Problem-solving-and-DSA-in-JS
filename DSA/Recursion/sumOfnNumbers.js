757c56c826c34d30a8a684a961ee4ea9b1d7d839
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-07-04 18:11:10 +0530

    Added another way of code for the problem

---
+++ DSA/Recursion/sumOfnNumbers.js
@@ -0,0 +1,30 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// Parametarised way
+// function sumOfNumbers(i, n) {
+
+//     if (i > n) {
+//         return 0;
+//     }
+//     return i + sumOfNumbers(i + 1, n)
+// }
+
+// Type 2
+
+function sumOfNumbers(i, sum) {
+
+    if (i < 1) {
+        console.log(sum)
+        return 0;
+    }
+    sumOfNumbers(i - 1, sum + i)
+}
+
+rl.question("Enter n: ", (n) => {
+    sumOfNumbers(Number(n),0)
+    rl.close();
+});
