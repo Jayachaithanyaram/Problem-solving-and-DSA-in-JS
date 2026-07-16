4049f543c6392da0363a55c64651cd2b6e1520e7
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-07-16 19:40:24 +0530

    Solved majorityElement problem using Boyer-Moore voting Algorithm

--- DSA/Hashing/majorityElement.js
+++ DSA/Hashing/majorityElement.js
@@ -1,16 +1,36 @@
-function majorityElement(arr) {
-    let map = new Map()
+// function majorityElement(arr) {
+//     let map = new Map()
+//     for (let num of arr) {
+//         if (map.has(num)) {
+//             map.set(num, map.get(num) + 1)
+//         } else {
+//             map.set(num, 1)
+//         }
+//     }
+//     for (let [key, value] of map) {
+//         if (value > Math.floor(arr.length / 2)) {
+//             return key
+//         }
+//     }
+// }
+// console.log(majorityElement([2, 2, 1, 1, 1, 1, 1, 2, 2]))
+
+//  Boyer-Moore voting Algorithm
+function majorityElement1(arr) {
+    let candidate = null
+    let count = 0
     for (let num of arr) {
-        if (map.has(num)) {
-            map.set(num, map.get(num) + 1)
+        if (count === 0) {
+            candidate = num;
+        }
+
+        if (num == candidate) {
+            count++
         } else {
-            map.set(num, 1)
+            count--
         }
     }
-    for (let [key, value] of map) {
-        if (value > Math.floor(arr.length / 2)) {
-            return key
-        }
-    }
+    return candidate
+
 }
-console.log(majorityElement([2, 2, 1, 1, 1, 1, 1, 2, 2]))
+console.log(majorityElement1([2, 2, 1, 1, 1, 1, 1, 2, 2]))
