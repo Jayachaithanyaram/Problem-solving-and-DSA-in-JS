ea020d3ba662614d59aef1fdf66895d8eb779159
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-07-20 20:02:48 +0530

    Solved the problem which is to remove the duplicates from the sorted array

---
+++ DSA/Arrays/removeDuplicates.js
@@ -0,0 +1,15 @@
+function removeDuplicates(arr) {
+    if (arr.length === 0) return 0;
+
+    let i = 0;
+
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[j] !== arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    return i + 1;
+}
+console.log(removeDuplicates([1,2,3,3,4,5]))
