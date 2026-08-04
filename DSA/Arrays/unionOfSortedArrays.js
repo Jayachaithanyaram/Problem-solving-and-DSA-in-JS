f08364701999ebce2d07533d49defe332f3594e0
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-04 18:31:17 +0530

    Updated the brute force method code in the problem

--- DSA/Arrays/unionOfSortedArrays.js
+++ DSA/Arrays/unionOfSortedArrays.js
@@ -1,11 +1,9 @@
 // Brute Force method
 function unionOfSortedArrays(arr1, arr2) {
     let set1 = new Set(arr1)
-    let set2 = new Set(arr2)
+    let set2 = new Set()
     for (let nums of arr2) {
-        if (set2.has(nums)) {
-            set1.add(nums)
-        }
+        set1.add(nums)
     }
     return [...set1]
 }
@@ -18,12 +16,12 @@ function unionOfSortedArrays1(arr1, arr2) {
     let unionArr = []
     while (i < arr1.length && j < arr2.length) {
         if (arr1[i] <= arr2[j]) {
-            if (unionArr.length == 0 || unionArr[unionArr.length-1] !== arr1[i]) {
+            if (unionArr.length == 0 || unionArr[unionArr.length - 1] !== arr1[i]) {
                 unionArr.push(arr1[i])
             }
             i++
         } else {
-            if (unionArr.length == 0 || unionArr[unionArr.length-1] !== arr2[j]) {
+            if (unionArr.length == 0 || unionArr[unionArr.length - 1] !== arr2[j]) {
                 unionArr.push(arr2[j])
             }
             j++
@@ -31,13 +29,13 @@ function unionOfSortedArrays1(arr1, arr2) {
 
     }
     while (j < arr2.length) {
-        if (unionArr.length == 0 || unionArr[unionArr.length-1] !== arr2[j]) {
+        if (unionArr.length == 0 || unionArr[unionArr.length - 1] !== arr2[j]) {
             unionArr.push(arr2[j])
         }
         j++
     }
     while (i < arr1.length) {
-        if (unionArr.length == 0 || unionArr[unionArr.length-1] !== arr1[i]) {
+        if (unionArr.length == 0 || unionArr[unionArr.length - 1] !== arr1[i]) {
             unionArr.push(arr1[i])
         }
         i++
