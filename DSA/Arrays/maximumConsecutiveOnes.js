d5229765eb9f7c36f8c8f2c03d3b10df50a7c687
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-05 11:14:19 +0530

    Solved maximumConsecutiveOnes problem #leetcode485

---
+++ DSA/Arrays/maximumConsecutiveOnes.js
@@ -0,0 +1,14 @@
+function maximumConsecutiveOnes(arr) {
+    let max = 0
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == 1) {
+            count++
+            max = Math.max(max, count)
+        } else {
+            count = 0
+        }
+    }
+    return max
+}
+console.log(maximumConsecutiveOnes([1, 1, 0, 1, 1, 1, 0, 1, 1]))
