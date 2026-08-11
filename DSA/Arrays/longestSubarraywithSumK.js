2882afbc47e34e6aa06e9c31fbda7e028c86e4fb
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-11 19:27:33 +0530

    Added optimal solution to the code which is solved using two pointer approach

--- DSA/Arrays/longestSubarraywithSumK.js
+++ DSA/Arrays/longestSubarraywithSumK.js
@@ -13,7 +13,7 @@ function longestSubarraywithSumK(arr, k) {
 console.log(longestSubarraywithSumK([1, 2, 3, 1, 1, 1, 1, 4, 2, 3], 3))
 
 // Better Solution
-function longestSubarrayWithSumK(arr, k) {
+function longestSubarrayWithSumK1(arr, k) {
 
     let map = new Map();
 
@@ -40,4 +40,27 @@ function longestSubarrayWithSumK(arr, k) {
     return maxLen;
 }
 
-console.log(longestSubarrayWithSumK([1, 2, 3, 1, 1, 1, 1, 4, 2, 3], 3));
+console.log(longestSubarrayWithSumK1([2, 0, 0, 3], 3));
+// [1, 2, 3, 1, 1, 1, 1, 4, 2, 3]
+
+//  Optimal Solution Two pointer Approach
+function longestSubarrayWithSumK2(arr, k) {
+    let n = arr.length;
+    let sum =0
+    let maxlen = 0
+    let left = 0
+    let right = 0
+    while (right < n) {
+        while (left <= right && sum > k) {
+            sum -= arr[left]
+            left++
+        }
+        if (sum == k) {
+            maxlen = Math.max(maxlen, right - left )
+        }
+        right++
+        if (right < n) sum += arr[right]
+    }
+    return maxlen
+}
+console.log(longestSubarrayWithSumK2([1, 2, 3, 1, 1, 1, 1, 4, 2, 3], 6))
