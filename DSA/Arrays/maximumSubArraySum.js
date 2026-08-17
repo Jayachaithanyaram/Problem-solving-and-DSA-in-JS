c145a879c035858d619f8cf682e360056bb654ca
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-17 18:13:33 +0530

    Solved the maximum Sub array sum problem using kadane's algorith m which is the optimal solution

--- DSA/Arrays/maximumSubArraySum.js
+++ DSA/Arrays/maximumSubArraySum.js
@@ -16,7 +16,7 @@ console.log(maximumSubArraySum([-2, -3, 4, -1, -2, 1, 5, -3]))
 // better solution
 
 function maximumSubArraySum1(arr) {
-    maxlength = null
+    maxlength = -Infinity
     for (let i = 0; i < arr.length; i++) {
         let sum = 0
         for (let j = i; j < arr.length; j++) {
@@ -28,4 +28,43 @@ function maximumSubArraySum1(arr) {
 }
 console.log(maximumSubArraySum1([-2, -3, 4, -1, -2, 1, 5, -3]))
 
+// Optimal Solution using kadane's algorithm to print the max sum
+function maximumSubArraySum2(arr) {
+    let sum = 0
+    let maxlen = -Infinity
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+        if (sum > maxlen) {
+            maxlen = sum
+        }
+        if (sum < 0) {
+            sum = 0
+        }
+    }
+      return maxlen
+}
+console.log(maximumSubArraySum2([-2, -3, 4, -1, -2, 1, 5, -3]))
 
+
+// Optimal Solution using kadane's Algorithm to print the maximum sum sub array
+function maximumSubArraySum3(arr) {
+    let sum = 0
+    let maxlen = -Infinity
+    let ansStart = -1
+    let ansEnd = -1
+    let start = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (sum == 0) start = i
+        sum += arr[i]
+        if (sum > maxlen) {
+            maxlen = sum
+            ansStart = start
+            ansEnd = i
+        }
+        if (sum < 0) {
+            sum = 0
+        }
+    }
+      return arr.slice(ansStart, ansEnd + 1)
+}
+console.log(maximumSubArraySum3([-2, -3, 4, -1, -2, 1, 5, -3]))
