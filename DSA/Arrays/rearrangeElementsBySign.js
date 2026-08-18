17ad10293ad28b05d8f85cf5362a5568d9e20eb8
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-18 18:55:34 +0530

    Solved the rearrange elements in the array by sign in better aproach

---
+++ DSA/Arrays/rearrangeElementsBySign.js
@@ -0,0 +1,36 @@
+// Brute Force Solution
+function rearrangeElementsBySign(arr) {
+    let posArr = []
+    let negArr = []
+    for (let num of arr) {
+        if (num > 0) {
+            posArr.push(num)
+        } else {
+            negArr.push(num)
+        }
+    }
+    for (let i = 0; i < (arr.length) / 2; i++) {
+        arr[2 * i] = posArr[i]
+        arr[2 * i + 1] = negArr[i]
+    }
+    return arr
+}
+console.log(rearrangeElementsBySign([3, 1, -2, -5, 2, -4]))
+
+// Better Solution
+function rearrangeElementsBySign1(arr) {
+    let rearr = []
+    let posArr = 0
+    let negArr = 1
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            rearr[posArr] = arr[i]
+            posArr += 2
+        } else if (arr[i] < 0) {
+            rearr[negArr] = arr[i]
+            negArr += 2
+        }
+    }
+    return rearr
+}
+console.log(rearrangeElementsBySign1([3, 1, -2, -5, 2, -4]))
