820e27fe3a32203803f0913f7e7737fc75e7e0cf
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-22 17:14:25 +0530

    Solved the optimal solution for the Leaders in the array problem

--- DSA/Arrays/leadersInArray.js
+++ DSA/Arrays/leadersInArray.js
@@ -11,3 +11,18 @@ function superiorElements(arr) {
     return leadArr
 }
 console.log(superiorElements([10, 22, 12, 3, 0, 6]))
+
+function superiorElements1(arr) {
+    let maxi = -Infinity
+    let n = arr.length
+    let count = 0
+    for (let i = n - 1; i >= 0; i--) {
+        if (arr[i] > maxi) {
+            arr[arr.length - 1 - count] = arr[i]
+            count++
+        }
+        maxi = Math.max(maxi, arr[i])
+    }
+    return count
+}
+console.log(superiorElements1([10, 22, 12, 3, 0, 6]))
