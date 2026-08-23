711fa9268d1be6e68b60b9fe26565c3e36dac2a2
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-23 17:54:08 +0530

    Solved the set Matrix Zeroes problem in Optimal solution #leetcode73

--- DSA/Arrays/setMatrixZeroes.js
+++ DSA/Arrays/setMatrixZeroes.js
@@ -59,7 +59,7 @@ function setMatrixZeroes1(matrix) {
     }
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (row[i] || col[j]) { 
+            if (row[i] || col[j]) {
                 matrix[i][j] = 0
             }
         }
@@ -74,4 +74,54 @@ let result1 = setMatrixZeroes1([
 
 for (let row of result1) {
     console.log(row.join(" "))
+}
+
+// Optimal Solution code
+function setMatrixZeroes2(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let col0 = 1;
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] === 0) {
+            col0 = 0;
+        }
+
+        for (let j = 1; j < n; j++) {
+
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if (matrix[0][0] === 0) {
+        for (let j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+    if (col0 === 0) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    return matrix;
+}
+
+let result2 = setMatrixZeroes2([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+]);
+
+for (let row of result2) {
+    console.log(row.join(" "));
 }
