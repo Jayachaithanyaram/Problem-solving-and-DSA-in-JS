3e92a47931d44ef64d433ddfe8ccfddd8352a9b0
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-24 17:12:22 +0530

    Solved the rotate Matrix to 90 degrees problem with brute force code

---
+++ DSA/Arrays/rotateMatrixImage.js
@@ -0,0 +1,22 @@
+// Brute Force Solution
+function rotateMatrix(matrix) {
+    let n = matrix.length
+    let ansMatrix = new Array(n)
+    for (let i = 0; i < n; i++) {
+        ansMatrix[i] = new Array(n)
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            ansMatrix[j][n - 1 - i] = matrix[i][j]
+        }
+    }
+    return ansMatrix
+}
+let result = rotateMatrix([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+for (let row of result) {
+    console.log(row.join(" "))
+}
