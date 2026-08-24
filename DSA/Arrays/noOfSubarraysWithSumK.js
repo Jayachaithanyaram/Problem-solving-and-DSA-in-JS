1f105b56c76556ba064f13302e520dc5ad8c3f75
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-24 20:32:47 +0530

    Solved the number of sub arrays with sum k problem with brute force code solution

---
+++ DSA/Arrays/noOfSubarraysWithSumK.js
@@ -0,0 +1,18 @@
+// Brute force solution
+function noOfSubarraysWithSumK(arr, k) {
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            let sum = 0
+            for (let k = i; k <= j; k++) {
+                sum += arr[k]
+            }
+            if (sum == k) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+console.log(noOfSubarraysWithSumK([1, 2, 3, -3, 1, 1, 1, 4, 2, -3], 3))
