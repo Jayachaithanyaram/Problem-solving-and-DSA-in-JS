ecaf02589ec7f48fd4a3f9f682d27ed870839e49
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-08-24 17:45:36 +0530

    Solved the rotate matrix image upto 90' degrees in optimal code solution #leetcode48

--- DSA/Arrays/rotateMatrixImage.js
+++ DSA/Arrays/rotateMatrixImage.js
@@ -20,3 +20,26 @@ let result = rotateMatrix([
 for (let row of result) {
     console.log(row.join(" "))
 }
+
+// Optimal Solution code
+function rotateMatrix1(matrix) {
+    let n = matrix.length
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] =
+                [matrix[j][i], matrix[i][j]];
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse()
+    }
+    return matrix
+}
+let result1 = rotateMatrix1([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+for (let row of result1) {
+    console.log(row.join(" "))
+}
