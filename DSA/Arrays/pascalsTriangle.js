71abcce10a441796a3c0b4d15049495053202088
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-09-07 19:25:40 +0530

    Solved and added brute force solution for printing the entire elements in a row

--- DSA/Arrays/pascalsTriangle.js
+++ DSA/Arrays/pascalsTriangle.js
@@ -22,10 +22,26 @@ console.log(pascalsTriangle(5, 3))
 // Optimized brute force solution
 function pascalsTriangle1(n, r) {
     let res = 1
-    for (let i = 0; i <r; i++) {
+    for (let i = 0; i < r; i++) {
         res *= (n - i)
-        res /= (i+1)
+        res /= (i + 1)
     }
     return res
 }
-console.log(pascalsTriangle1(4,2))
+console.log(pascalsTriangle1(4, 2))
+
+// Brute force solution for printing the entire row elements
+function pascalsTriangle2(n) {
+    // function pascalsTriangle1(n, c) {
+    //     let res = 1
+    //     for (let i = 0; i < c; i++) {
+    //         res *= (n - i)
+    //         res /= (i + 1)
+    //     }
+    //     return res
+    // }
+    for (let c = 1; c <= n; c++) {
+        console.log(pascalsTriangle1(n - 1, c - 1))
+    }
+}
+pascalsTriangle2(6)
