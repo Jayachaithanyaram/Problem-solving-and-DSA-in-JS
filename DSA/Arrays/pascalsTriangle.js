bb13031a61fa4cb7c7d27924bea508f02face7a6
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-09-07 21:53:02 +0530

    Solved and added the code of solving the pascal's triangle problem and printing the entire triangle using optimal solution

--- DSA/Arrays/pascalsTriangle.js
+++ DSA/Arrays/pascalsTriangle.js
@@ -1,65 +1,65 @@
-// // Pascals triangle for where row and column are given and the element is to be returned
-// // Brute force solution
-// function rowColumnElement(r, c) {
-//     let num = 1
-//     for (let i = 1; i < r; i++) {
-//         num *= i
-//     }
-//     let den1 = 1
-//     for (let i = 1; i < c; i++) {
-//         den1 *= i
-//     }
-//     let bal = r - c
-//     let den2 = 1
-//     for (let i = 1; i <= bal; i++) {
-//         den2 *= i
-//     }
-//     let result = num / (den1 * den2)
-//     return result
-// }
-// console.log(rowColumnElement(5, 3))
+// Pascals triangle for where row and column are given and the element is to be returned
+// Brute force solution
+function rowColumnElement(r, c) {
+    let num = 1
+    for (let i = 1; i < r; i++) {
+        num *= i
+    }
+    let den1 = 1
+    for (let i = 1; i < c; i++) {
+        den1 *= i
+    }
+    let bal = r - c
+    let den2 = 1
+    for (let i = 1; i <= bal; i++) {
+        den2 *= i
+    }
+    let result = num / (den1 * den2)
+    return result
+}
+console.log(rowColumnElement(5, 3))
 
-// // Optimized brute force solution
-// function rowColumnElement1(n, r) {
-//     let res = 1
-//     for (let i = 0; i < r; i++) {
-//         res *= (n - i)
-//         res /= (i + 1)
-//     }
-//     return res
-// }
-// console.log(rowColumnElement1(4, 2)) /*we have to pass the arguments as n-1 and r-1 , which means 5-1, 3-1*/
+// Optimized brute force solution
+function rowColumnElement1(n, r) {
+    let res = 1
+    for (let i = 0; i < r; i++) {
+        res *= (n - i)
+        res /= (i + 1)
+    }
+    return res
+}
+console.log(rowColumnElement1(4, 2)) /*we have to pass the arguments as n-1 and r-1 , which means 5-1, 3-1*/
 
-// // Brute force solution for printing the entire row elements
-// function rowElements(n) {
-//     // function pascalsTriangle1(n, c) {
-//     //     let res = 1
-//     //     for (let i = 0; i < c; i++) {
-//     //         res *= (n - i)
-//     //         res /= (i + 1)
-//     //     }
-//     //     return res
-//     // }
-//     for (let c = 1; c <= n; c++) {
-//         console.log(rowElements(n - 1, c - 1))
-//     }
-// }
-// rowElements(6)
+// Brute force solution for printing the entire row elements
+function rowElements(n) {
+    // function pascalsTriangle1(n, c) {
+    //     let res = 1
+    //     for (let i = 0; i < c; i++) {
+    //         res *= (n - i)
+    //         res /= (i + 1)
+    //     }
+    //     return res
+    // }
+    for (let c = 1; c <= n; c++) {
+        console.log(rowElements(n - 1, c - 1))
+    }
+}
+rowElements(6)
 
-// // optimized solution for printing the entire elements in a row
-// function ncr(n) {
-//     let ans = 1
-//     console.log(ans)
-//     for (let i = 1; i < n; i++) {
-//         ans *= (n - i)
-//         ans /= i
-//         console.log(ans)
-//     }
-// }
-// ncr(6)
+// optimized solution for printing the entire elements in a row
+function ncr1(n) {
+    let ans = 1
+    console.log(ans)
+    for (let i = 1; i < n; i++) {
+        ans *= (n - i)
+        ans /= i
+        console.log(ans)
+    }
+}
+ncr1(6)
 
 
-// Brute force solution for printing the entire pascals triangle
+Brute force solution for printing the entire pascals triangle
 function ncr(n, r) {
     let ans = 1
     for (let i = 0; i < r; i++) {
@@ -70,9 +70,9 @@ function ncr(n, r) {
 }
 function pascalsTriangle(n) {
     let ans = []
-    for (let row = 1; row <=n; row++) {
+    for (let row = 1; row <= n; row++) {
         let temparr = []
-        for (let column = 1; column <=row; column++) {
+        for (let column = 1; column <= row; column++) {
             temparr.push(ncr(row - 1, column - 1))
         }
         ans.push(temparr.join(" "))
@@ -80,3 +80,22 @@ function pascalsTriangle(n) {
     return ans
 }
 console.log(pascalsTriangle(5))
+
+// Optimal solution for printing the pascals triangle
+
+function ncr1(n) {
+    let res = []
+    for (let row = 1; row <= n; row++) {
+        let templist = []
+        let ans = 1
+        templist.push(ans)
+        for (let column = 1; column < row; column++) {
+            ans *= (row - column)
+            ans /= column
+            templist.push(ans)
+        }
+        res.push(templist)
+    }
+    return res
+}
+console.log(ncr1(5))
