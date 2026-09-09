0b114d8b231af6bb51419d226d68e368bd2a4602
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-09-09 19:42:01 +0530

    solved the 3sum problem with Optimal code of solution

--- DSA/Arrays/3sum.js
+++ DSA/Arrays/3sum.js
@@ -44,3 +44,33 @@ function threeSum1(arr) {
     return res
 }
 console.log(threeSum1([-1, 0, 1, 2, -1, -4]))
+
+// Optimal Solution
+function threeSum2(arr) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i > 0 && arr[i] == arr[i - 1]) {
+            continue
+        }
+        let j = i + 1
+        let k = arr.length - 1
+
+        while (j < k) {
+            let sum = arr[i] + arr[j] + arr[k]
+            if (sum < 0) {
+                j++
+            } else if (sum > 0) {
+                k--
+            } else {
+                res.push([arr[i], arr[j], arr[k]])
+                j++
+                k--
+                while (j < k && arr[j] == arr[j - 1]) { j++ }
+                while (j < k && arr[k] == arr[k + 1]) { k-- }
+            }
+        }
+
+    }
+    return res
+}
+console.log(threeSum2([-2, -2, -2, -1, -1, -1, 0, 0, 0, 2, 2, 2, 2]))
