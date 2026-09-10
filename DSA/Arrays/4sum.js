6092175b4e0914292cd6c924ccdf998d8e3df621
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-09-10 21:50:34 +0530

    Solved 4sum problem with Optimal solution of code #leetcode18

--- DSA/Arrays/4sum.js
+++ DSA/Arrays/4sum.js
@@ -48,3 +48,36 @@ function fourSum1(arr, target) {
     return res
 }
 console.log(fourSum1([1, 0, -1, 0, -2, 2], 0))
+
+
+// Optimal solution
+function fourSum2(arr, target) {
+    let res = []
+    arr.sort((a, b) => a - b)
+    for (let i = 0; i < arr.length; i++) {
+        if (i > 0 && arr[i] == arr[i - 1]) continue
+        for (let j = i + 1; j < arr.length; j++) {
+            if (j != i + 1 && arr[j] == arr[j - 1]) continue
+            let k = j + 1
+            let l = arr.length - 1;
+            while (k < l) {
+                let sum = arr[i] + arr[j] + arr[k] + arr[l]
+                if (sum < target) {
+                    k++
+                } else if (sum > target) {
+                    l--
+                }
+                else {
+                    res.push([arr[i], arr[j], arr[k], arr[l]])
+                    k++
+                    l--
+                    while (k < l && arr[k] == arr[k - 1]) k++
+                    while (k < l && arr[l] == arr[l + 1]) l--
+                }
+            }
+        }
+    }
+    return res
+}
+console.log(fourSum2([1, 0, -1, 0, -2, 2], 0))
+// console.log(fourSum1([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5], 8))
